3b4d963c1f51160be130d0e938f6009a0e18adab
Wing Poon <wypoon1@gmail.com>
2022-03-27 22:09:22 -0700

    Updated PDFs

--- ipynb_to_html_pdf.sh
+++ ipynb_to_html_pdf.sh
@@ -5,5 +5,5 @@ do
 FNAME="${FILE##*/}"
 FROOT="${FNAME%.*}"
 jupyter-nbconvert "$FILE" --to html --output "../docs/$FROOT.html"
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --print-to-pdf-no-header --print-to-pdf="pdf/$FROOT.pdf" "html/$FROOT.html"
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --print-to-pdf-no-header --print-to-pdf="pdf/$FROOT.pdf" "docs/$FROOT.html"
 done
